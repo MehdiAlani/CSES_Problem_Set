50b3ce28e4bb02da3e43b6f60f8b38cfd7ce77d6
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-10-02 17:40:14 -0200

    2 oct 23

--- 2_MissingNumber.c
+++ 2_MissingNumber.c
@@ -1,4 +1,28 @@
 #include <stdio.h>
+int CharTonum(char Num){
+    switch(Num){
+        case '0':
+            return 0;
+        case '1':
+            return 1;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+    }
+}
 int main(){
     long long int n = 0,index=0;
     do{
@@ -6,20 +30,20 @@ int main(){
     scanf("%d",&n);
     }while(200000 < n || n < 2);
     int ListNumber[n];
+    char Msg[2*n-1];
+    for (int i = 0; i<)
     ListNumber[0]=-1;
     for (int i=1;i<n-1;i++){
         ListNumber[i]=i;
     }
-    for(int i =0;i<n-1;i++){
-        do{ 
-        fflush(stdin);
-        scanf("%d",&index);
-        }while (ListNumber[index]==-1);
+    scanf("%s",Msg);
+    for(int i =0; i < 2*n-1;i=i+2){
+        ListNumber[CharTonum(Msg[i])]=-1;
+        printf("%cd ",Msg[i]);
     }
     for (int i=1;i<n-1;i++){
         if(ListNumber[i]!=-1){
             printf("%d",ListNumber[i]);
-            return 0;
         }
     }
     return 1;
